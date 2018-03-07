d9bb7d107dd143ac2ce181252753ebb2dac25a4a
champsamui8@gmail.com <champsamui8@gmail.com>
2018-03-08 01:04:43 +0700

    cdcd

--- database/migrations/2018_02_02_082924_create_log_table.php
+++ database/migrations/2018_02_02_082924_create_log_table.php
@@ -16,7 +16,7 @@ class CreateLogTable extends Migration
         Schema::create('log', function (Blueprint $table) {
             $table->increments('Log_ID');
             $table->integer('official_ID')->unsigned();
-            $table->foreign('official_ID')->references('official_ID')->on('official');
+                
             $table->string('table_log');
             $table->integer('project_log');
             $table->string('Log_Event');
