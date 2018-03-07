ad846bc75e5ad5315fd475127297c755717afb6a
champsamui8@gmail.com <champsamui8@gmail.com>
2018-03-08 01:00:21 +0700

    dsds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,12 +13,26 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $time =Carbon::now('Asia/Bangkok');
+      App\official::create([
 
+                        'official_Name' => 'Admin',
+                        'official_Email' => 'admin@admin.com',
+                        'official_Role' => '0',
+                        'official_cotton' => '0',
+                        'official_Password' => '123456',
+                        'info' => 'จัดการ',
+                        'product' => 'จัดการ',
+                        'hotnews' => 'จัดการ',
+                        'activity' => 'จัดการ',
+                        'prison' => 'จัดการ',
+                        'offcreated_at' => "" . $time->year. "-" . $time->month . "-" . $time->day . " " . $time->hour . ":" . $time->minute. ":" . $time->second . "",
+                        'offupdated_at' => "" . $time->year. "-" . $time->month . "-" . $time->day . " " . $time->hour . ":" . $time->minute. ":" . $time->second . "",
+                      ]);
 
 
                       App\log::create([
 
-                                      
+                                        'official_ID' => '6',
                                         'table_log' => 'official',
                                         'project_log' => '0',
                                         'Log_Event' => 'เข้าสู่ระบบ',
