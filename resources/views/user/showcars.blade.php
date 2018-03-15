142d8e090774012c9409d93582797f0dbf3397b8
champsamui8@gmail.com <champsamui8@gmail.com>
2018-03-15 22:43:24 +0700

    xsxs

--- resources/views/user/showcars.blade.php
+++ resources/views/user/showcars.blade.php
@@ -224,7 +224,7 @@
                       <div class="col-md-12 text-center">
                             <h5>ถ่ายภาพ</h5>
 </div>
-                        <div class="col-md-6 ">
+                        <div class="col-md-6 col-md-offset-1">
                               <div id="my_camera"></div>
 </div>
                       <div class="col-md-6 pull-right">
