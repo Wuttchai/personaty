f70c58201eef947f198265f762b9fe066d5394dc
champsamui8@gmail.com <champsamui8@gmail.com>
2018-03-17 01:54:31 +0700

    dsds

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -13,8 +13,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
-      URL::forceShcheme('https');
-      
+      URL::forceSchema('https');
+      $this->app['request']->server->set('HTTPS','on');
       Validator::extend('image64', function ($attribute, $value, $parameters, $validator) {
      $type = explode('/', explode(':', substr($value, 0, strpos($value, ';')))[1])[1];
      if (in_array($type, $parameters)) {
