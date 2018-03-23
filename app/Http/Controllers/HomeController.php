25201a4bf07ea39eb3ce2051fdee4483e929a341
champsamui8@gmail.com <champsamui8@gmail.com>
2018-03-24 00:18:07 +0700

    sasa

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -74,7 +74,7 @@ class HomeController extends Controller
 $question = \App\question::join('users','question.User_ID','=','users.User_ID')
 ->join('questiondetail','questiondetail.ques_id','=','question.ques_id')
 ->select('question.ques_id',DB::raw('count(questiondetail.quesde_id) as user_count'), 'question.ques_name','question.ques_detail', 'question.ques_date','users.User_Name', 'question.ques_type')
-->GROUPBY('question.ques_id')
+->GROUPBY('question.ques_id','users.User_Name')
 ->orderBy('ques_date', 'desc')->paginate(5);
 
 
