15536e3a35273b524b8475d20fa9241c4557dd11
champsamui8@gmail.com <champsamui8@gmail.com>
2018-03-25 23:58:37 +0700

    dsdsds

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -94,7 +94,7 @@ $cartItem = Cart::add($request->id, $product[0]->Pro_Name, $request->quantity, $
 
 $question = \App\question::join('users','question.User_ID','=','users.User_ID')
 ->join('questiondetail','questiondetail.ques_id','=','question.ques_id')
-->select('question.ques_id',DB::raw('count(questiondetail.quesde_id) as user_count'), 'question.ques_name','question.ques_detail', 'question.ques_date','users.User_ID','users.User_Name', 'question.ques_type','question.ques_id','question.official_ID')
+->select('question.ques_id',DB::raw('count(questiondetail.quesde_id) as user_count'), 'question.ques_name','question.ques_detail', 'question.ques_date','users.User_ID','users.User_Name', 'question.ques_type','question.ques_id','question.quesde_owner')
 ->GROUPBY('question.ques_id','users.User_ID')
 ->orderBy('ques_date', 'desc')->paginate(5);
 
