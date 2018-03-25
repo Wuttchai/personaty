317ab95106628072e8998a2e4c362bea5f41206c
champsamui8@gmail.com <champsamui8@gmail.com>
2018-03-26 01:18:24 +0700

    dsds

--- resources/views/user/webboard.blade.php
+++ resources/views/user/webboard.blade.php
@@ -108,7 +108,7 @@
 
   @if(Session::get('idoffice') != null)
   @if($user->User_ID == '1')
-  <?php dd(Session::get('idoffice')); ?>
+
   <span class="glyphicon glyphicon-cog " v-on:click="open()" title="แก้ไขข้อมูลกระทู้"></span>
   @endif
   @endif
