ac595e2de951fe3c06f8821ee983ac462c666222
champsamui8@gmail.com <champsamui8@gmail.com>
2018-03-28 03:41:26 +0700

    dsds

--- resources/views/official/pdf.blade.php
+++ resources/views/official/pdf.blade.php
@@ -1,8 +1,8 @@
 
 
 
-<object data="http://project3.test/pdf/<?php echo $file[0]->doc_file ?>" type="application/pdf" width="1300px" height="750px">
-    <embed src="http://project3.test/pdf/<?php echo $file[0]->doc_file ?>" type="application/pdf">
-        <p>This browser does not support PDFs. Please download the PDF to view it: <a href="http://project3.test/pdf/<?php echo $file[0]->doc_file ?>">Download PDF</a>.</p>
+<object data="https://project3.test/pdf/<?php echo $file[0]->doc_file ?>" type="application/pdf" width="1300px" height="750px">
+    <embed src="https://project3.test/pdf/<?php echo $file[0]->doc_file ?>" type="application/pdf">
+        <p>This browser does not support PDFs. Please download the PDF to view it: <a href="https://project3.test/pdf/<?php echo $file[0]->doc_file ?>">Download PDF</a>.</p>
     </embed>
 </object>
