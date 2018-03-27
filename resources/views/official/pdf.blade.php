b907501f37e788c33eb32f2368b1b85cc7be88e2
champsamui8@gmail.com <champsamui8@gmail.com>
2018-03-28 03:44:22 +0700

    cxcx

--- resources/views/official/pdf.blade.php
+++ resources/views/official/pdf.blade.php
@@ -1,8 +1,8 @@
 
 
 
-<object data="https://project3.test/pdf/<?php echo $file[0]->doc_file ?>" type="application/pdf" width="1300px" height="750px">
-    <embed src="https://project3.test/pdf/<?php echo $file[0]->doc_file ?>" type="application/pdf">
-        <p>This browser does not support PDFs. Please download the PDF to view it: <a href="https://project3.test/pdf/<?php echo $file[0]->doc_file ?>">Download PDF</a>.</p>
+<object data="https://prisonayuthaya.herokuapp.com/pdf/<?php echo $file[0]->doc_file ?>" type="application/pdf" width="1300px" height="750px">
+    <embed src="https://prisonayuthaya.herokuapp.com/pdf/<?php echo $file[0]->doc_file ?>" type="application/pdf">
+        <p>This browser does not support PDFs. Please download the PDF to view it: <a href="https://prisonayuthaya.herokuapp.com/pdf/<?php echo $file[0]->doc_file ?>">Download PDF</a>.</p>
     </embed>
 </object>
