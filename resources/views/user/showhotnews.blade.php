9b1b92a63749492ca3d2397b2f3b57815cbdc551
champsamui8@gmail.com <champsamui8@gmail.com>
2018-03-30 04:21:40 +0700

    กำกำำ

--- resources/views/user/showhotnews.blade.php
+++ resources/views/user/showhotnews.blade.php
@@ -24,7 +24,8 @@
               <div class="col-md-12 text-center">
  <img src="{{ asset('hotnew/' . $hotnew[0]->Hotnews_img) }}" class="img-responsive" style="width: 800px; height: 500px;" allowfullscreen>
 <br>
-<h4 class="text-center"> {{ Hotnews_name }}</h4>
+<h4 class="text-center"> {{ $hotnew[0]->Hotnews_name }}</h4>
+<h5> {{ $hotnew[0]->datefirst }} - {{ $hotnew[0]->datelast }}</h5>
 <br>
 <h5> {{ $hotnew[0]->Hotnews_detail }}</h5>
 
