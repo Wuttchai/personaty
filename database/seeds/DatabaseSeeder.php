8d621a66e8db4360a672843965290fe165c2f731
champsamui8@gmail.com <champsamui8@gmail.com>
2018-04-24 18:51:44 +0700

    dsds

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,12 +13,12 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
       $time =Carbon::now('Asia/Bangkok');
-  
+    
                       App\User::create([
                                         'User_Name' => 'เจ้าหน้าที่',
                                         'email' => 'admin@admin.com',
                                         'address_id' => 0,
-                                        'User_Tel' => '01111111111',
+
                                         'password' => bcrypt('123456'),
 
                                       ]);
